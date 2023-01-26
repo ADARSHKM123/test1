c85dc1c59554680dda9d86c53f214ddadfffbb67
ADARSH <67700780+ADARSHKM123@users.noreply.github.com>
2023-01-26 12:47:45 +0530

    Update test1.js
    corrected

--- test1.js
+++ test1.js
@@ -1,23 +1,31 @@
-function longestcmnSubstr(str1,str2){
-    let longest = "";
-    let current = "";
-
-    for(let i=0; i<str1.length;i++){
-        for(let j=0;j<str2.legth;j++){
-            if(str1[i] === str2[j]){
-                current += str1[i];
-                if(current.length > longest.length ) {
-                    longest = current
-                }
-            }else{
-       
-                current = "";
-            }
-        }
-    }
-    return longest;
+function longestCommonSubstring(str1, str2) {
+  let longest = "";
+  let current = "";
+  let dp = new Array(str1.length + 1);
+  for (let i = 0; i <= str1.length; i++) {
+    dp[i] = new Array(str2.length + 1);
+    for (let j = 0; j <= str2.length; j++) {
+      dp[i][j] = 0;
+    }
+  }
+  let maxLength = 0;
+  let end = 0;
+  for (let i = 1; i <= str1.length; i++) {
+    for (let j = 1; j <= str2.length; j++) {
+      if (str1[i - 1] === str2[j - 1]) {
+        dp[i][j] = dp[i - 1][j - 1] + 1;
+        if (dp[i][j] > maxLength) {
+          maxLength = dp[i][j];
+          end = i - 1;
+        }
+      }
+    }
+  }
+  if (maxLength === 0) {
+    return "";
+  }
+  return str1.substring(end - maxLength + 1, end + 1);
 }
 
-
-console.log(longestcmnSubstr("abcdef","abc"));
+console.log(longestCommonSubstring("abcdef", "abc"));
 
